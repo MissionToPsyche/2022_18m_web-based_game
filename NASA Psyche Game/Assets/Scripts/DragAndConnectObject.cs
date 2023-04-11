6572bc81864cb62749aa46aeccda181566bf5585
Nico <nico.platt@cox.net>
2023-04-11 00:18:05 -0700

    Added LineObject to serve as extendable wire
    Needs fixes related to the base of the object. Will likely need to scrap the usage of a the WireObject prefab in order to solve. Removing this prefab will assist in setting material colors, as well.

--- NASA Psyche Game/Assets/Scripts/DragAndConnectObject.cs
+++ NASA Psyche Game/Assets/Scripts/DragAndConnectObject.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DragAndConnectObject : MonoBehaviour
 {
-    public SpriteRenderer wireStraight;
+    //public SpriteRenderer wireStraight;
+    public LineRenderer line;
 
     Vector3 startPos;
     Vector3 startPoint;
@@ -25,10 +26,10 @@ public class DragAndConnectObject : MonoBehaviour
 
         //check for connections
         Collider2D[] colliders = Physics2D.OverlapCircleAll(newPos, .2f);
-        foreach(Collider2D collider in colliders)
+        foreach (Collider2D collider in colliders)
         {
             //if not self, update
-            if(collider.gameObject != gameObject)
+            if (collider.gameObject != gameObject)
             {
                 UpdateWire(collider.transform.position);
 
@@ -56,12 +57,20 @@ public class DragAndConnectObject : MonoBehaviour
         transform.position = newPos;
 
         //Calculate and Update the direction the wire head should be facing
-        Vector3 direction = newPos - startPoint;
-        transform.right = direction * transform.lossyScale.x;
+        //Vector3 direction = newPos - startPoint;
+        //transform.right = direction * transform.lossyScale.x;
 
         //scales
         float dist = Vector2.Distance(startPoint, newPos);
-        wireStraight.size = new Vector2(dist, wireStraight.size.y);
+        //wireStraight.size = new Vector2(dist, wireStraight.size.y);
+
+        ////// NEW STUFFS //////
+        //Updates extendable wire
+        Vector3 frontLine = new Vector3(gameObject.transform.position.x - .1f, gameObject.transform.position.y, gameObject.transform.position.z);
+        Vector3 backLine = new Vector3(gameObject.transform.position.x - .4f, gameObject.transform.position.y, gameObject.transform.position.z);
+
+        line.SetPosition(3, frontLine);
+        line.SetPosition(2, backLine);
     }
 
     void SuccesfulConnection(Collider2D matchingWire)
