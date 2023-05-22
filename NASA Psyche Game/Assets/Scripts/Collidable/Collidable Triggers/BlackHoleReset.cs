4f206e19ceb15e86ca8df09eef57c0830e9bc1dd
kklam50 <kyleklam2009@gmail.com>
2023-05-22 01:52:18 -0700

    Added black hole functionality

--- NASA Psyche Game/Assets/Scripts/Collidable/Collidable Triggers/BlackHoleReset.cs
+++ NASA Psyche Game/Assets/Scripts/Collidable/Collidable Triggers/BlackHoleReset.cs
@@ -2,12 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class BlackHoleReset : Collidable {
+    public Rigidbody2D player;
+
+    [SerializeField] private float gravitationalForce = 0.5f;
+
     protected override void OnCollide(Collider2D coll) {
         if (coll.name == "Psychenaut") {
             // play some sort of animation for the screen to reset here
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // Debug.Log("contacting black hole");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Since transform.position is a Vector3 but bird.position is a Vector2 you now have to cast
+        var directionOfPlayerFromBlackHole = ((Vector2) transform.position - player.position).normalized;
+        var vectorDistanceFromBlackHole = ((Vector2) transform.position - player.position);
+        var distanceFromBlackHole = Math.Sqrt(Math.Pow(vectorDistanceFromBlackHole.x, 2) + Math.Pow(vectorDistanceFromBlackHole.y, 2));
+        // Debug.Log("Distance: " + distanceFromBlackHole);
+        // Debug.Log("x val: " + directionOfPlayerFromBlackHole.x);
+        // Debug.Log("y val: " + directionOfPlayerFromBlackHole.y);
+
+        if (distanceFromBlackHole <= 0.75) {
+            // Adds the force towards the center
+            player.AddForce(directionOfPlayerFromBlackHole * gravitationalForce);
+        }
+        else {
+            // player.AddForce(new Vector2(0f, 0f));
+            player.velocity = Vector2.zero;
         }
     }
 }
