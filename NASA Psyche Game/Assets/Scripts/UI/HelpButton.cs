a92b867604459f04c47006a3cc50319ab16f515e
kklam50 <kyleklam2009@gmail.com>
2023-05-23 22:32:24 -0700

    Added controls menu to warehouse UI

--- NASA Psyche Game/Assets/Scripts/UI/HelpButton.cs
+++ NASA Psyche Game/Assets/Scripts/UI/HelpButton.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class WiringMinigameWarningManager : MonoBehaviour
-{
+public class HelpButton : MonoBehaviour {
     public GameObject diaBox;
-    public GameObject wiringMinigameTrigger;
+    public GameObject helpButton;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +28,11 @@ public class WiringMinigameWarningManager : MonoBehaviour
         diaBox.SetActive(false);
     }
 
-    public void TempDeactivateTrigger() {
-        wiringMinigameTrigger.SetActive(false);
-        StartDialogue();
-        Invoke("showObject", 5.0f);
+    public void ShowButton() {
+        helpButton.SetActive(true);
     }
 
-    void showObject() {
-        wiringMinigameTrigger.SetActive(true);
+    public void HideButton() {
+        helpButton.SetActive(false);
     }
 }
