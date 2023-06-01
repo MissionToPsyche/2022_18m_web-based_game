9fcb3ca67ef8d218592219b901a637d5879fa8d1
kklam50 <kyleklam2009@gmail.com>
2023-05-31 23:19:05 -0700

    Added poster popup eastereggs

---
+++ NASA Psyche Game/Assets/Scripts/PosterManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PosterManager : MonoBehaviour
+{
+    public GameObject diaBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diaBox.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && diaBox.activeSelf == true) {
+            EndDialogue();
+        }
+    }
+
+    public void StartDialogue() {
+        diaBox.SetActive(true);
+    }
+
+    public void EndDialogue() {
+        diaBox.SetActive(false);
+    }
+}
